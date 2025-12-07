6cab371d567e22433b5c5ca464bf4cb61a08e11d
unknown <you@example.com>
2025-12-07 15:05:55 +0100

    Correct migrate members

--- database/migrations/2025_11_02_153221_add_created_by_to_members_table.php
+++ database/migrations/2025_11_02_153221_add_created_by_to_members_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('members', function (Blueprint $table) {
             //
-            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
         });
     }
 
